8162dedf6be95f3c6157efb5a7bcb87373986bab
Chris Atanasian <catanasian@gmail.com>
2015-12-20 14:43:57 -0800

    Get questions

--- scraper_example.py
+++ scraper_example.py
@@ -4,10 +4,11 @@ import time
 
 class ScraperExample(scrapy.Spider):
     name = "scraper_example"
-    allowed_domains = ['quora.com', 'answers.yahoo.com']
-    start_urls = ['https://www.answers.yahoo.com']
+    allowed_domains = ["quora.com", "answers.yahoo.com"]
+    start_urls = ["https://www.answers.yahoo.com"]
 
     SLEEP_TIME = 0.75
+    CSS_QUESTION = ".Fz-14.Fw-b.Clr-b.Wow-bw.title"
 
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -20,10 +21,13 @@ class ScraperExample(scrapy.Spider):
             self.scroll_down()
             time.sleep(self.SLEEP_TIME)
 
+    def get_questions(self):
+        return [i.get_attribute('text') for i in self.driver.find_elements_by_css_selector(self.CSS_QUESTION)]
+
     def parse(self, response):
         self.driver.get(response.url)
-        self.scroll_down_n_times(25)
+        self.scroll_down_n_times(1)
 
-        # begin scraping
+        self.get_questions()
 
         self.driver.close()
