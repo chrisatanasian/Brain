253c786a6cddd66300c59e8c6bec04e964decc99
Chris Atanasian <catanasian@gmail.com>
2015-12-20 13:23:49 -0800

    Add scrolling to bottom of page

--- scraper_example.py
+++ scraper_example.py
@@ -1,19 +1,29 @@
 import scrapy
 from selenium import webdriver
+import time
 
 class ScraperExample(scrapy.Spider):
     name = "scraper_example"
     allowed_domains = ['quora.com', 'answers.yahoo.com']
     start_urls = ['https://www.answers.yahoo.com']
 
+    SLEEP_TIME = 0.75
+
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
+
+    def scroll_down(self):
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    def scroll_down_n_times(self, n):
+        for i in range(n):
+            self.scroll_down()
+            time.sleep(self.SLEEP_TIME)
 
     def parse(self, response):
         self.driver.get(response.url)
+        self.scroll_down_n_times(25)
 
-        while True:
-            # scroll down to the bottom of the page
-            # begin scraping
+        # begin scraping
 
         self.driver.close()
