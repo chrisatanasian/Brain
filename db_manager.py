f831c831fdbb894974487efafc92ec5b540e717e
Chris Atanasian <catanasian@gmail.com>
2015-12-21 10:22:40 -0800

    Add method to add question to DB

--- db_manager.py
+++ db_manager.py
@@ -9,5 +9,16 @@ class DbManager:
             if not model.table_exists():
                 db.create_tables([model])
 
+    @staticmethod
+    def add_question(question):
+        try:
+            db_question = Question.create(content=question.content,
+                                          upvotes=question.upvotes,
+                                          category=question.category,
+                                          timestamp=datetime.fromtimestamp(question.created_utc))
+            db_question.save()
+        except peewee.IntegrityError:
+            print question.content + ' already in database'
+
 if __name__ == '__main__':
     DbManager.create_tables([Question, Answer])
