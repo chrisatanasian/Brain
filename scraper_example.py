ce0c6943fe24d731a80fc1942591eae3b1fb5763
Chris Atanasian <catanasian@gmail.com>
2015-12-29 14:42:43 -0800

    Add method to get dates

--- scraper_example.py
+++ scraper_example.py
@@ -10,6 +10,7 @@ class ScraperExample(scrapy.Spider):
     SLEEP_TIME      = 0.75
     CSS_QUESTION    = '.Fz-14.Fw-b.Clr-b.Wow-bw.title'
     CSS_BEST_ANSWER = '.desc.Fz-13.Lh-18'
+    CSS_DATE        = '.Clr-888.Fz-12.Lh-18'
 
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -28,13 +29,22 @@ class ScraperExample(scrapy.Spider):
     def get_best_answers(self):
         return [ba.text for ba in self.driver.find_elements_by_css_selector(self.CSS_BEST_ANSWER)]
 
+    def get_dates(self):
+        return [d.text.encode('utf8').split('\xb7')[-1] for d in self.driver.find_elements_by_css_selector(self.CSS_DATE)]
+
     def parse(self, response):
         self.driver.get(response.url)
         self.scroll_down_n_times(1)
 
         questions    = self.get_questions()
-        best_answers = self.get_best_answers()
+        time.sleep(self.SLEEP_TIME)
 
-        assert len(questions) == len(best_answers)
+        best_answers = self.get_best_answers()
+        time.sleep(self.SLEEP_TIME)
+
+        dates        = self.get_dates()
+        time.sleep(self.SLEEP_TIME)
+
+        assert len(questions) == len(best_answers) == len(dates)
 
         self.driver.close()
