599cf2f6fbf0020f3ae42960ec146391be11c260
Chris Atanasian <catanasian@gmail.com>
2015-12-29 15:02:16 -0800

    Add method for getting categories

--- ya_scraper.py
+++ ya_scraper.py
@@ -7,10 +7,10 @@ class ScraperExample(scrapy.Spider):
     allowed_domains = ['quora.com', 'answers.yahoo.com']
     start_urls      = ['https://www.answers.yahoo.com']
 
-    SLEEP_TIME      = 0.75
-    CSS_QUESTION    = '.Fz-14.Fw-b.Clr-b.Wow-bw.title'
-    CSS_BEST_ANSWER = '.desc.Fz-13.Lh-18'
-    CSS_DATE        = '.Clr-888.Fz-12.Lh-18'
+    SLEEP_TIME            = 0.75
+    CSS_QUESTION          = '.Fz-14.Fw-b.Clr-b.Wow-bw.title'
+    CSS_BEST_ANSWER       = '.desc.Fz-13.Lh-18'
+    CSS_DATE_AND_CATEGORY = '.Clr-888.Fz-12.Lh-18'
 
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -29,8 +29,11 @@ class ScraperExample(scrapy.Spider):
     def get_best_answers(self):
         return [ba.text for ba in self.driver.find_elements_by_css_selector(self.CSS_BEST_ANSWER)]
 
-    def get_dates(self):
-        return [d.text.encode('utf8').split('\xb7')[-1] for d in self.driver.find_elements_by_css_selector(self.CSS_DATE)]
+    def get_question_dates(self):
+        return [d.text.encode('utf8').split('\xb7')[-1] for d in self.driver.find_elements_by_css_selector(self.CSS_DATE_AND_CATEGORY)]
+
+    def get_categories(self):
+        return [c.text.encode('utf8').split('\xb7')[-2] if len(c.text.encode('utf8').split('\xb7')) > 1 else '' for c in self.driver.find_elements_by_css_selector(self.CSS_DATE_AND_CATEGORY)]
 
     def parse(self, response):
         self.driver.get(response.url)
@@ -42,9 +45,12 @@ class ScraperExample(scrapy.Spider):
         best_answers = self.get_best_answers()
         time.sleep(self.SLEEP_TIME)
 
-        dates        = self.get_dates()
+        dates        = self.get_question_dates()
         time.sleep(self.SLEEP_TIME)
 
-        assert len(questions) == len(best_answers) == len(dates)
+        categories   = self.get_categories()
+        time.sleep(self.SLEEP_TIME)
+
+        assert len(questions) == len(best_answers) == len(dates) == len(categories)
 
         self.driver.close()
